0a49204ba3a967aa7b2181ebdb0dbfbea3879c0d
Lhasbellaoui Sabrina <sabrina.asbellaoui@gmail.com>
2023-01-19 20:25:27 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Form from "./Form";
+// import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Form.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+
+export const Form = () => {
+  return (
+    <div>
+      <form>
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label for="inputName4">Name</label>
+            <input
+              type="name"
+              class="form-control"
+              id="inputName4"
+              placeholder="Name"
+            ></input>
+          </div>
+          <div class="form-group col-md-6">
+            <label for="inputName4">Surname</label>
+            <input
+              type="name"
+              class="form-control"
+              id="inputName4"
+              placeholder="Surname"
+            ></input>
+          </div>
+          <div class="form-group col-md-6">
+            <label for="inputEmail4">Email</label>
+            <input
+              type="email"
+              class="form-control"
+              id="inputEmail4"
+              placeholder="Email"
+            ></input>
+          </div>
+          <div class="form-group col-md-6">
+            <label for="inputPassword4">Password</label>
+            <input
+              type="password"
+              class="form-control"
+              id="inputPassword4"
+              placeholder="Password"
+            ></input>
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="inputAddress">Address</label>
+          <input
+            type="text"
+            class="form-control"
+            id="inputAddress"
+            placeholder="1234 Main St"
+          ></input>
+        </div>
+        <div class="form-group">
+          <label for="inputAddress2">Address 2</label>
+          <input
+            type="text"
+            class="form-control"
+            id="inputAddress2"
+            placeholder="Apartment, studio, or floor"
+          ></input>
+        </div>
+        <div class="form-row">
+          <div class="form-group col-md-6">
+            <label for="inputCity">City</label>
+            <input type="text" class="form-control" id="inputCity"></input>
+          </div>
+          <div class="form-group col-md-4">
+            <label for="inputState">State</label>
+            <select id="inputState" class="form-control">
+              <option selected>Choose...</option>
+              <option>...</option>
+            </select>
+          </div>
+          <div class="form-group col-md-2">
+            <label for="inputZip">Zip</label>
+            <input type="text" class="form-control" id="inputZip"></input>
+          </div>
+        </div>
+        <div class="form-group">
+          <div class="form-check">
+            <input
+              class="form-check-input"
+              type="checkbox"
+              id="gridCheck"
+            ></input>
+            <label class="form-check-label" for="gridCheck">
+              Accept terms and conditions
+            </label>
+          </div>
+        </div>
+        <button type="submit" class="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+export default Form;
